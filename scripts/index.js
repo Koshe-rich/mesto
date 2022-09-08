36477d587d2f8de5d05091187348b3957f130766
Koshe <raptaste@yandex.ru>
2022-09-08 13:00:39 +0300

    feat: review changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ let popup = body.querySelector('.popup');
 let popupBtnClose = popup.querySelector('.popup__btn-close');
 let popupBtnOpen = body.querySelector('.profile__edit-button');
 let form = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_description');
+let nameInput = document.querySelector('.popup__input_line_name');
+let jobInput = document.querySelector('.popup__input_line_description');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
@@ -12,16 +12,20 @@ function formSubmitHandler(event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    form.addEventListener('submit', closePopup);
 };
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+};
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+};
+
+popupBtnOpen.addEventListener('click', openPopup);
+
+popupBtnClose.addEventListener('click', closePopup);
 
 form.addEventListener('submit', formSubmitHandler);
 
-popupBtnOpen.addEventListener('click', function openPopup() {
-    popup.classList.add('popup_opened');
-    nameInput.textContent = profileName.value;
-    jobInput.textContent = profileJob.value;
-});
-
-popupBtnClose.addEventListener('click', function closePopup() {
-    popup.classList.remove('popup_opened');
-});
