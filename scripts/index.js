37fbb55865e67cf7418382c3a6e980fe6981b3be
Koshe <raptaste@yandex.ru>
2022-09-22 11:56:40 +0300

    Last commit

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,44 @@ let jobInput = document.querySelector('.popup__input_line_description');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
+const element = document.querySelector('.element');
+const elementPlace = document.querySelector('#element-place');
+const img = elementPlace.content.querySelector('.img');
+const elementPlaceName = elementPlace.content.querySelector('.element__place-name');
+const elementBtnLike  = elementPlace.content.querySelector('.element__btn-like');
+
+console.log(img )
+
+
+const initialCards = [
+    {
+        name: 'Дюна Пилат',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Беловежская пуща',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Сан-Франциско-де-Кампече',
+        link: 'https://images.unsplash.com/photo-1623843740978-8a18154b8e5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80'
+    },
+    {
+        name: 'Озеро Брайес',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Самарканд',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Королевство Мустанг',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
 function formSubmitHandler(event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
@@ -15,10 +53,10 @@ function formSubmitHandler(event) {
     closePopup();
 };
 
-function openPopup () {
-popup.classList.add('popup_opened'); 
-nameInput.textContent = profileName.value; 
-jobInput.textContent = profileJob.value;
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.textContent = profileName.value;
+    jobInput.textContent = profileJob.value;
 };
 
 function closePopup() {
