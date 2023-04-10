0cf4faf2a742c2b4ba7a91ce0ae2c182cd9b98aa
Koshe <shelkunovka@gmail.com>
2023-04-10 16:11:11 +0300

    Добавить класс Section

---
+++ scripts/Section.js
@@ -0,0 +1,19 @@
+class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  render() {
+    this._container.innerHTML = '';
+    this._items.forEach((item) => {
+      const element = this._renderer(item);
+      this.addItem(element);
+    });
+  }
+
+  addItem(element) {
+    this._container.appendChild(element);
+  }
+}
