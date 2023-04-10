4c5a3f6e9caf1a6302985d11a0132040f802a439
Koshe <shelkunovka@gmail.com>
2023-04-10 16:11:51 +0300

    Добавить класс Popup

---
+++ scripts/Popup.js
@@ -0,0 +1,35 @@
+export class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    const closeButton = this._popup.querySelector('.popup__close');
+    const overlay = this._popup.querySelector('.popup__overlay');
+
+    closeButton.addEventListener('click', () => {
+      this.close();
+    });
+
+    overlay.addEventListener('click', () => {
+      this.close();
+    });
+  }
+}
