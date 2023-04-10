9b9e5f6e13a6f79c1da2d663d8a67832768e8f00
Koshe <shelkunovka@gmail.com>
2023-04-10 16:12:43 +0300

    Добавить класс PopupWithForm

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,30 @@
+class PopupWithForm extends Popup {
+  constructor(popupSelector, submitCallback) {
+    super(popupSelector);
+    this._submitCallback = submitCallback;
+    this._formElement = this._popupElement.querySelector('.popup__form');
+    this._inputList = this._formElement.querySelectorAll('.popup__input');
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputList.forEach(input => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      this._submitCallback(this._getInputValues());
+      this.close();
+    });
+  }
+
+  close() {
+    super.close();
+    this._formElement.reset();
+  }
+}
