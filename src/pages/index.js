09a0b360404e462bc79f242af491b5363c2700b8
Koshe <shelkunovka@gmail.com>
2023-04-17 13:37:05 +0300

    ревью. переместить index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import {
   cfg
-} from './scripts/validation';
-import {Card} from './components/Card.js'
-import { FormValidator } from './components/FormValidator.js';
-import { Section } from './components/Section.js';
-import { PopupWithImage } from './components/PopupWithImage.js';
-import { PopupWithForm } from './components/PopupWithForm.js';
-import { UserInfo } from './components/UserInfo.js'
-import "./pages/index.css"
-import { initialCards } from './scripts/dataCards.js';
+} from '../scripts/validation.js';
+import {Card} from '../components/Card.js'
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js'
+import "./index.css"
+import { initialCards } from '../scripts/dataCards.js';
 
 
 const body = document.querySelector('.page');
